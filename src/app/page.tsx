19e232d52a7553e6408ab87f9e781de756b1abf8
Dirk Stahlecker <dirkstahlecker24@gmail.com>
2023-12-18 23:10:20 -0700

    adding stuff back

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return <>
+    <h1>Dirk Stahlecker</h1>
+    <br/>
+    <h3><a href="https://www.dirkstahleckerracing.com/" target="_blank">Racing site</a></h3>
+    <h3><a href="https://www.trackchasing.dirkstahlecker.com/" target="_blank">Trackchasing site</a></h3>
+  </>
+}
